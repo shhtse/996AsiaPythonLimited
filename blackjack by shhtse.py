86f7f892dd51e840ef6690bb88cb07e144770764
shhtse <79773402+shhtse@users.noreply.github.com>
2021-04-01 23:53:03 +0100

    Update blackjack by shhtse.py

--- blackjack by shhtse.py
+++ blackjack by shhtse.py
@@ -1,33 +1,38 @@
 import random
-player_hand=[]
-dealer_hand=[]
+
+player_hand = []
+dealer_hand = []
 deck = []
 
+
 def shuffle():
-    a= 1
-    while a<5:
+    a = 1
+    while a < 5:
         for i in range(13):
             if i == 1:
                 deck.append('A')
-            elif i  == 11 :
+            elif i == 11:
                 deck.append('J')
-            elif i  == 12 :
+            elif i == 12:
                 deck.append('Q')
-            elif i  == 0 :
+            elif i == 0:
                 deck.append('K')
             else:
                 deck.append(i)
         a += 1
-        
+
     random.shuffle(deck)
+
 
 def hit():
     card = deck.pop()
     player_hand.append(card)
 
+
 def dealer_hit():
     card = deck.pop()
     dealer_hand.append(card)
+
 
 def cal():
     player_pt = 0
@@ -43,6 +48,7 @@ def cal():
             else:
                 player_pt += 10
     return player_pt
+
 
 def dealer_cal():
     dealer_pt = 0
@@ -61,18 +67,19 @@ def dealer_cal():
                 dealer_pt += 10
     return dealer_pt
 
+
 shuffle()
-for i in range(2):hit()
+for i in range(2): hit()
 cal()
-print('Player hand :', player_hand,'. Player point =',cal())
-for i in range(2):dealer_hit()
+print('Player hand :', player_hand, '. Player point =', cal())
+for i in range(2): dealer_hit()
 dealer_cal()
-print('Dealer hand : [?], ',dealer_hand[1:],'. Dealer point =',dealer_cal())
+print('Dealer hand : [?], ', dealer_hand[1:], '. Dealer point =', dealer_cal())
 decision = input('Hit or stand?(H) or (S)').lower()
 while decision != 's':
     hit()
     cal()
-    print('Player hand :', player_hand,'. Player point =',cal())
+    print('Player hand :', player_hand, '. Player point =', cal())
     if cal() > 21:
         print('Bust!')
         break
@@ -83,16 +90,16 @@ if cal() <= 21:
     while dealer_cal() < 17:
         dealer_hit()
         dealer_cal()
-    print('Dealer hand : ',dealer_hand,'. Dealer point =',dealer_cal())
-    if cal() == dealer_cal() :
-      print('打和啦Super!!')
+    print('Dealer hand : ', dealer_hand, '. Dealer point =', dealer_cal())
+    if cal() == dealer_cal():
+        print('打和啦Super!!')
     elif cal() == 21:
-      print('BlackJack! You win.')
+        print('BlackJack! You win.')
     elif dealer_cal() == 21:
-      print('Dealer has a BlackJack')
-    elif dealer_cal() >21:
-      print('Dealer bust, you win')
+        print('Dealer has a BlackJack')
+    elif dealer_cal() > 21:
+        print('Dealer bust, you win')
     elif cal() < dealer_cal():
-      print('You loss')
+        print('You loss')
     else:
         print('Yon win')
