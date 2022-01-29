257adf0cb5517f2f0765e7730d245415a32fb582
Wu Ka Cheong <79691025+kcwu229@users.noreply.github.com>
2022-01-29 13:33:46 +0800

    2nd update
    1. Add color text for easy recognition and reminds
    2. Add try except for prevent crashing

--- Algorithmic Trading Project/test.py
+++ Algorithmic Trading Project/test.py
@@ -23,10 +23,13 @@ Storing the code status: append code into code
 
 codes = []                                 # store table being store from the web
 letters = list(string.ascii_uppercase)
+
+print("\033[1;32;1m Executing Mission 1 ... \033[0m\n")
+
 for letter in letters:
     url = f"https://eoddata.com/stocklist/NASDAQ/{letter}.htm"
     req = requests.get(url)
-    soup = BeautifulSoup(req.content)
+    soup = BeautifulSoup(req.content, 'lxml')
     
     # filter with special pattern: stockquote/NASDAQ/...
     tags = soup.find_all("a", href=re.compile("/stockquote/NASDAQ/"))
@@ -35,50 +38,74 @@ for letter in letters:
         if (t.string is not None):
             if (len(t.string) >= 2 or t.string != "HOME"):
                 codes.append(t.string)
+                
+print("\033[1;32;1m Mission 1 Complete ! \033[0m\n")
+
+
+print("\033[1;32;1m Executing Mission 1 ... \033[0m\n")
 
 #-----------------------------------------------------------------------------------------------
 
 # Part 2: Access data from pandas
 
 count = 0
+errorCount = 0
 endDate = datetime.now()                   # Due to current time system (your computer)
 startDate = endDate - timedelta(days=1)    # Two days before current date (your computer)
 
 str_endDate = endDate.strftime("%Y-%m-%d")         # date object to string   
 str_startDate = startDate.strftime("%Y-%m-%d")     
+errorCode = []
 
 codeStatus = {
-    "code":[],  
-    "codeOpen ($)":[],
-    "codeClose ($)":[],
-    "codeVolume":[]
+    "code": [],
+    "codeOpen ($)": [],
+    "codeClose ($)": [],
+    "codeVolume": []
     }
 
+print("\033[1;32;1m Executing Mission 2 ... \033[0m\n")
 
 for code in codes:
-    count += 1
-    data = web.DataReader(code, "yahoo", str_startDate, str_endDate)
-    # 要最新果日 (SCRARPE TIME: 香港時間 2022/01/29 01:20 AM, 但用"2022-01-28", "2022-01-29" 會顯示出27, 28 日的價(可能是時差問題))
+    try:
+        count += 1
+        data = web.DataReader(code, "yahoo", str_startDate, str_endDate)
+        # 要最新果日 (SCRARPE TIME: 香港時間 2022/01/29 01:20 AM, 但用"2022-01-28", "2022-01-29" 會顯示出27, 28 日的價(可能是時差問題))
+        
+        # Attracting the number of rows from the dataset
+        for i in range(data.shape[0]):
+            stockOpen = data["Open"][i]
+            stockClose = data["Close"][i]
+            stockVolume = data["Volume"][i]
     
-    # Attracting the number of rows from the dataset
-    for i in range(data.shape[0]):
-        stockOpen = data["Open"][i]
-        stockClose = data["Close"][i]
-        stockVolume = data["Volume"][i]
-   
-        codeStatus["code"].append(code)
-        codeStatus["codeOpen ($)"].append(stockOpen)
-        codeStatus["codeClose ($)"].append(stockClose)
-        codeStatus["codeVolume"].append(stockVolume)
-    
+            codeStatus["code"].append(code)
+            codeStatus["codeOpen ($)"].append(stockOpen)
+            codeStatus["codeClose ($)"].append(stockClose)
+            codeStatus["codeVolume"].append(stockVolume)
+        
+        print(f" Successful: {code}")
+            
+    except:
+        print(f"\033[1;31m Has probelems on -----{code} \033[0m\n")
+        errorCode.append(code)
+        
+        
+print("\033[1;32;1m Mission 2 Complete ! \033[0m\n")
+
 #-----------------------------------------------------------------------------------------------
 
-
 # Part 3: Convert Dict to DataFrame and export to CSV file
+
+print("\033[1;32;1m Executing Mission 3 ... \033[0m\n")
 
 df = pd.DataFrame(codeStatus)
 df.to_csv(f"stockList_{str_endDate}.csv", index=False)
 
-print("Mission complete !")
-print(f"Number of stock access: {count}")
 
+print(f"Number of stock access: {count}")
+print(f"Number of error encounter while scraping: {len(errorCount)}")
+print(errorCount)
+
+
+print("\033[1;32;1m Mission 3 Complete ! \033[0m\n")
+print("\033[1;32;1m Finish !!! \033[0m\n")
